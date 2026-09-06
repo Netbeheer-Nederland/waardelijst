7983f8f806e439ce5d7f0ad32aa55c73f4bb95ef
Bart Kleijngeld <bartkl@protonmail.com>
2026-09-06 20:10:31 +0200

    First version seems to ready to go

--- build.py
+++ build.py
@@ -6,44 +6,8 @@ import shutil
 import jinja2
 from pyoxigraph import *
 
-NAL_FILE = "src/netbeheerder/netbeheerder.skos.ttl"
+NAL_FILES_DIR = "src"
 ANTORA_COMPONENT_DIR = "_docs-adoc"
-
-# Read and query
-nal = Store()
-for triple in parse(path=NAL_FILE, format=RdfFormat.TURTLE):
-    nal.add(triple)
-
-# Parse query and serialize to dictionary
-scheme = json.loads(nal.query('''
-    PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
-    PREFIX dcterms: <http://purl.org/dc/terms/>
-    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-    SELECT *
-    WHERE {
-        ?id a skos:ConceptScheme ;
-            dcterms:subject ?subject ;
-            skos:notation ?name ;
-            dcterms:title ?title .
-    }
-''').serialize(format=QueryResultsFormat.JSON))["results"]["bindings"][0]
-
-print(scheme)
-
-terms = sorted(json.loads(nal.query('''
-    PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
-    PREFIX foaf: <http://xmlns.com/foaf/0.1/>
-    PREFIX dcterms: <http://purl.org/dc/terms/>
-    PREFIX adms: <http://www.w3.org/ns/adms#>
-    SELECT *
-    WHERE {
-        ?id a skos:Concept ;
-            skos:prefLabel ?prefLabel ;
-            foaf:homepage ?homepage ;
-            adms:status ?status ;
-            skos:notation ?notation .
-    }
- ''').serialize(format=QueryResultsFormat.JSON))["results"]["bindings"], key=lambda t: t["id"]["value"])
 
 # Create Antora component
 shutil.rmtree(ANTORA_COMPONENT_DIR)
@@ -53,16 +17,57 @@ os.makedirs(ANTORA_COMPONENT_DIR)
 with open(os.path.join(ANTORA_COMPONENT_DIR, "antora.yml"), "wt") as f:
     f.write(textwrap.dedent(f'''
         name: ROOT
-        title: {scheme["title"]["value"]}
+        title: NBNL Name Authority Lists
         version: ~
     ''').strip())
 
-## Write page
-pages_dir = os.path.join(ANTORA_COMPONENT_DIR, "modules", "ROOT", "pages")
-os.makedirs(pages_dir, exist_ok=True)
+for nal_file in os.listdir(NAL_FILES_DIR):
+    nal_file_path = os.path.join(NAL_FILES_DIR, nal_file)
+    # Read and query
+    nal = Store()
+    for triple in parse(path=nal_file_path, format=RdfFormat.TURTLE):
+        nal.add(triple)
 
-adoc_template = jinja2.Environment(loader=jinja2.FileSystemLoader(".")).get_template("name-authority-list.adoc.jinja2")
-adoc = adoc_template.render(scheme=scheme, base_iri="https://modellen.netbeheernederland.nl/name-authority-lists/netbeheerder#", terms=terms)  # TODO: Base IRI should be parsed from document.
+    # Parse query and serialize to dictionary
+    scheme = json.loads(nal.query('''
+        PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+        PREFIX dcterms: <http://purl.org/dc/terms/>
+        PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+        SELECT *
+        WHERE {
+            ?id a skos:ConceptScheme ;
+                dcterms:subject ?subject ;
+                skos:notation ?name ;
+                dcterms:title ?title .
+        }
+    ''').serialize(format=QueryResultsFormat.JSON))["results"]["bindings"][0]
 
-with open(os.path.join(pages_dir, scheme["name"]["value"] + ".adoc"), "wt") as f:
-    f.write(adoc)
+    terms = sorted(json.loads(nal.query('''
+        PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+        PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+        PREFIX dcterms: <http://purl.org/dc/terms/>
+        PREFIX adms: <http://www.w3.org/ns/adms#>
+        SELECT *
+        WHERE {
+            ?id a skos:Concept ;
+                skos:prefLabel ?prefLabel ;
+                foaf:homepage ?homepage ;
+                adms:status ?status ;
+                skos:notation ?code .
+        }
+     ''').serialize(format=QueryResultsFormat.JSON))["results"]["bindings"], key=lambda t: t["id"]["value"])
+
+    ## Write page
+    attachments_dir = os.path.join(ANTORA_COMPONENT_DIR, "modules", "ROOT", "attachments")
+    pages_dir = os.path.join(ANTORA_COMPONENT_DIR, "modules", "ROOT", "pages")
+    os.makedirs(pages_dir, exist_ok=True)
+    os.makedirs(attachments_dir, exist_ok=True)
+
+    adoc_template = jinja2.Environment(loader=jinja2.FileSystemLoader(".")).get_template("name-authority-list.adoc.jinja2")
+    adoc = adoc_template.render(scheme=scheme, terms=terms)
+
+    with open(os.path.join(pages_dir, scheme["name"]["value"] + ".adoc"), "wt") as f:
+        f.write(adoc)
+
+    ## Copy artifacts
+    shutil.copy(nal_file_path, os.path.join(ANTORA_COMPONENT_DIR, "modules", "ROOT", "attachments"))
